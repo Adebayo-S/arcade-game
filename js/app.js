beed5dfbf95c56eb6182e733b39bc61b717cade5
Adebayo Samuel <eladebayoor@gmail.com>
2022-04-29 21:02:15 +0100

    [feat]: enemy spawning at random feature added

--- js/app.js
+++ js/app.js
@@ -1,21 +1,55 @@
 // ENEMIES
 var Enemy = function(enemy = {})
 {
-    let rndm = Math.random();
+    this.enemy = enemy;
     this.x = enemy.position.x;
     this.y = enemy.position.y;
-    this.speed = (enemy.speed === 0) ? 0 : enemy.speed * rndm;
+    this.speed = enemy.speed * (Math.floor(Math.random() * 3) + 1);
     this.sprite = enemy.sprite;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    let isBugFlipped;
+    if (this.sprite === 'images/enemy-bug-flip.png')
+    {
+        isBugFlipped = true;
+        this.x -= this.speed * dt;
+        if (this.x < -50)
+            this.x = 505;
+    }
+    else
+    {
+        isBugFlipped = false;
+        this.x += this.speed * dt;
+        if (this.x > 505)
+            this.x = -50;
+    }
+
+    let newBug = bugs.random();
+    let isSpawnReady = (Math.random() < 0.005);
+
+    if (this.x > 300 && isSpawnReady && allEnemies.length < 6)
+        allEnemies.push(new Enemy(newBug));
+
+    if (allEnemies.length == 6)
+    {
+        for (let i = 3; i < allEnemies.length; i++)
+        {
+            if ((allEnemies[i].x < -30) && isBugFlipped)
+                allEnemies.splice(i, 1);
+            else if ((allEnemies[i].x > 500) && !isBugFlipped)
+                allEnemies.splice(i, 1);
+        }
+    }
+};
+    //position reset
 
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+
 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -60,32 +94,32 @@ Player.prototype.handleInput = function(keyDown) {
 
 let bugs = [
     {
-        position: { x: 0, y: 55},
+        position: { x: -50, y: 60},
         speed: 100,
         sprite: 'images/enemy-bug.png'
     },
     {
-        position: { x: 0, y: 55},
+        position: { x: 500, y: 145},
+        speed: 100,
+        sprite: 'images/enemy-bug-flip.png'
+    },
+    {
+        position: { x: -50, y: 225},
         speed: 100,
         sprite: 'images/enemy-bug.png'
     }
 ]
-let bug = {
-    position: { x: 0, y: 55},
-    speed: 100,
-    sprite: 'images/enemy-bug.png'
-};
 
-let rock = {
-    position: { x: 0, y: 0},
-    speed: 0,
-    sprite: 'images/Rock.png'
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random() * this.length))];
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [ new Enemy(bug)/*, new Enemy(rock), new Enemy(bug)*/];
+var allEnemies = [ new Enemy(bugs[0]), new Enemy(bugs[1]), new Enemy(bugs[2])];
 // Place the player object in a variable called player
+
+
 var char = "boy";
 var player = new Player(char);
 
--- js/engine.js
+++ js/engine.js
@@ -173,6 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+        'images/enemy-bug-flip.png',
         'images/char-boy.png',
         'images/char-pink-girl.png',
         'images/char-horn-girl.png',
